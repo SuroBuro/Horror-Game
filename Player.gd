fbe7c055c2b930765ffcba781e80074cf874cd09
SuroBuro <134256681+SuroBuro@users.noreply.github.com>
2023-07-02 21:07:44 +0530

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 
+export var acceleration = 20
 export var gravity = -40
 export var speed = 70
 export var friction = 60
@@ -8,9 +9,11 @@ export var mouse_sensitivity = 0.1
 export var jump_impulse = 30
 
 var velocity = Vector3.ZERO
+var direction = Vector3()
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel") and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -26,26 +29,32 @@ func _unhandled_input(event):
 		
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	
+	direction = Vector3()
 
 	if Input.is_action_pressed("right"):
-		direction.x += 1
+		direction += transform.basis.x
 	if Input.is_action_pressed("left"):
-		direction.x -= 1
+		direction -= transform.basis.x
 	if Input.is_action_pressed("backward"):
-		direction.z += 1
+		direction += transform.basis.z
 	if Input.is_action_pressed("forward"):
-		direction.z -= 1
+		direction -= transform.basis.z
 
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
+	if direction != Vector3():
+		$Camera/AnimationPlayer.play("Head_bob")
 		
-
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	velocity = velocity.linear_interpolate(direction*speed , acceleration *delta )
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	
+	
+	
+	apply_gravity(delta)
 	
 func apply_gravity(delta):
 		velocity.y += gravity * delta
 		velocity.y = clamp(velocity.y, gravity, jump_impulse)
 		
+
+
